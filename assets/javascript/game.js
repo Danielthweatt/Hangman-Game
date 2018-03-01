d2978aae2d269201c9aa23c1bb85a42f0a8a064d
Danielthweatt <dthweatt192@yahoo.com>
2018-03-01 10:45:52 -0600

    worked on js, added links to index

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let wins = 0;
+let words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+let triesLeft = 12;
+let word = '';
+let key = '';
+
+const game = {
+    setWord: function(param) {
+        word = words[Math.floor(Math.random() *10)];
+        document.
+        return word;
+    },
+    testKeys: function(param1, param2) {
+
+    },
+    playGame: function() {
+
+    }, 
+};
+
+game.playGame();
