ed9fa9ceba56fcda530892233c7de2265161041f
Danielthweatt <dthweatt192@yahoo.com>
2018-03-02 14:35:48 -0600

    more work on index and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-let wins;
+const words = ['frodo', 'samwise', 'meriadoc', 'peregrin', 'gandalf', 'aragorn', 'gimli', 'legolas', 'boromir', 'sauron'];
+let wins = 0;
 let word;
 let wordBlank;
 let triesLeft;
@@ -12,7 +12,6 @@ let indexTracker;
 
 const game = {
     setWord: function(wordsArray) {
-        wins = 0;
         word = wordsArray[Math.floor(Math.random() * 10)];
         wordBlank = '';
         triesLeft = 15;
@@ -23,11 +22,16 @@ const game = {
         document.getElementById('wins').innerHTML = wins;
         document.getElementById('word').innerHTML = wordBlank;
         document.getElementById('triesLeft').innerHTML = triesLeft;
+        document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
     },
-    testKeys: function(currentWord, currentKey) {
+    testKeys: function (currentWord, currentKey) {
         if (currentWord.search(currentKey) >= 0) {
             wordBlankArray = wordBlank.split('');
-            wordBlankArray[currentWord.search(currentKey)] = currentKey;
+            if (currentWord.search(currentKey) === 0) {
+                wordBlankArray[0] = currentKey.toUpperCase();
+            } else {
+                wordBlankArray[currentWord.search(currentKey)] = currentKey;
+            }
             indexTracker = currentWord.search(currentKey);
             currentWord = currentWord.slice(currentWord.search(currentKey) + 1);
             while (currentWord.search(currentKey) >= 0) {
@@ -37,28 +41,33 @@ const game = {
             }
             wordBlank = wordBlankArray.join('');
             document.getElementById('word').innerHTML = wordBlank;
-        } else {
-            triesLeft = triesLeft - 1;
-            lettersGuessed = lettersGuessed.concat(keyStroke, ' ');
-            document.getElementById('triesLeft').innerHTML = triesLeft;
+        } else if (lettersGuessed.search(currentKey) < 0) {
+            lettersGuessed = lettersGuessed.concat(currentKey, ' ');
             document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
         }
+        triesLeft = triesLeft - 1;
+        document.getElementById('triesLeft').innerHTML = triesLeft;
     },
     playGame: function() {
-        while (true) {
-            setWord(words)
-            while (true) {
-                keyStroke = ;
-                testKeys(word, keyStroke)
-                if (triesLeft === 0) {
-                    alert("Woops! Try again!")
-                    break
-                }
-                if (wordBlank === word) {
-                    alert("Yay!!! You got it!")
-                    break
-                }
+        game.setWord(words)
+        document.onkeydown = function () {
+            keyStroke = event.key;
+            game.testKeys(word, keyStroke)
+            if (triesLeft === 0) {
+                setTimeout(function() {
+                    alert("Woops! Sucks to suck! Try again!")
+                    game.playGame()
+                }, 400)
+            }   
+            if (wordBlank.toLowerCase() === word) {
+                wins = wins + 1;
+                setTimeout(function() {
+                    alert("Yay! You did it! Play Again!")
+                    game.playGame()
+                }, 400)
             }
         }
-    }, 
+    }
 };
+
+game.playGame()
