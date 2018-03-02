08d0e9147510a1021f3db1de419b1a55b1fb7ce9
Danielthweatt <dthweatt192@yahoo.com>
2018-03-01 21:34:49 -0600

    worked on index and javascript more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,41 @@
 'use strict';
 
 let wins = 0;
-let words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-let triesLeft = 12;
-let word = '';
-let key = '';
+const words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+let triesLeft = 15;
+let word;
+let key;
+let wordBlank;
+let wordBlankArray;
+let lettersGuessed;
 
 const game = {
-    setWord: function(param) {
-        word = words[Math.floor(Math.random() *10)];
-        let wordBlank = "_";
-        for (let i = word.length; i > 1; i--) {
-            wordBlank = wordBlank.concat(" _");
+    setWord: function(wordsArray) {
+        word = wordsArray[Math.floor(Math.random() * 10)];
+        for (let i = word.length; i > 0; i--) {
+            wordBlank = wordBlank.concat('_');
         }
-        document.getElementById("word").innerHTML = wordBlank;
+        document.getElementById('word').innerHTML = wordBlank;
         return word;
     },
-    testKeys: function(param1, param2) {
-
+    testKeys: function(currentWord, keyStroke) {
+        if (currentWord.search(keyStroke) >= 0) {
+            wordBlankArray = wordBlank.split('');
+            while (currentWord.search(keyStroke) >= 0) {
+                wordBlankArray[currentWord.search(keyStroke)] = keyStroke;
+                currentWord = currentWord.slice(currentWord.search(keyStroke))
+            }
+            wordBlank = wordBlankArray.join('');
+            document.getElementById('word').innerHTML = wordBlank;
+        } else {
+            triesLeft = triesLeft - 1;
+            lettersGuessed = lettersGuessed.concat(keyStroke, ' ');
+            document.getElementById('triesLeft').innerHTML = triesLeft;
+            document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
+        }
+        return triesLeft; 
     },
     playGame: function() {
 
     }, 
 };
-
-game.playGame();
