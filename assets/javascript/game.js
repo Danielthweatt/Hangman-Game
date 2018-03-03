3b8faeb2808de3eb72e62b874fb4b8eed80218a8
Danielthweatt <dthweatt192@yahoo.com>
2018-03-03 13:38:49 -0600

    worked on js, added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const words = ['frodo', 'samwise', 'meriadoc', 'peregrin', 'gandalf', 'aragorn', 'gimli', 'legolas', 'boromir', 'sauron'];
+// variables
+const words = [['frodo', './assets/images/Elijah_Wood_as_Frodo_Baggins.png'], 
+['samwise', './assets/images/6d370cb9e22da130dac7c794fbf2368a.gif'], 
+['meriadoc', './assets/images/7824820.png'], 
+['peregrin', './assets/images/v1J3W7w3-8043.jpg'], 
+['gandalf', './assets/images/1-101222121109-lp.jpg'], 
+['aragorn', './assets/images/Aragorn-in-The-Two-Towers-aragorn-34519327-419-425.jpg'], 
+['gimli', './assets/images/14573-25242.gif'], 
+['legolas', './assets/images/Legolas_-_in_Two_Towers.png'], 
+['boromir', './assets/images/cftthfzgjsvnimu97z2w.jpg'], 
+['sauron', './assets/images/Eye-of-Sauron,-Lord-of-the-Rings,-Return-of-the-King.jpg']];
 let wins = 0;
 let word;
 let randomNumber;
@@ -11,20 +21,25 @@ let keyStroke;
 let wordBlankArray;
 let indexTracker;
 
+// game object
 const game = {
+    // function to set up a new round with a random word
     setWord: function(wordsArray) {
         randomNumber = Math.floor(Math.random() * 10);
-        word = wordsArray[randomNumber];
+        word = wordsArray[randomNumber][0];
         wordBlank = '';
         triesLeft = 12;
         lettersGuessed = '';
         for (let i = word.length; i > 0; i--) {
             wordBlank = wordBlank.concat('_');
         }
+        document.getElementById('secondHeader').innerHTML = '';
+        document.getElementById('img').setAttribute('src', '')
         document.getElementById('word').innerHTML = wordBlank;
         document.getElementById('triesLeft').innerHTML = triesLeft;
         document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
     },
+    // function to test key input and process it
     testKeys: function (currentWord, currentKey) {
         if (currentWord.search(currentKey) >= 0 && wordBlank.search(currentKey) < 0) {
             wordBlankArray = wordBlank.split('');
@@ -49,6 +64,7 @@ const game = {
             document.getElementById('triesLeft').innerHTML = triesLeft;
         }
     },
+    // main function that runs the game - will run indefinitely
     playGame: function() {
         game.setWord(words)
         document.onkeydown = function () {
@@ -56,14 +72,15 @@ const game = {
             game.testKeys(word, keyStroke)
             if (triesLeft === 0) {
                 document.onkeydown = function() {};
-                setTimeout(function() {game.playGame()}, 1500)
+                setTimeout(function() {game.playGame()}, 5000)
             }   
             if (wordBlank.toLowerCase() === word) {
                 document.onkeydown = function() {};
                 document.getElementById('secondHeader').innerHTML = wordBlank;
+                document.getElementById('img').setAttribute('src', words[randomNumber][1])
                 wins = wins + 1;
                 document.getElementById('wins').innerHTML = wins;
-                setTimeout(function() {game.playGame()}, 1500)
+                setTimeout(function() {game.playGame()}, 5000)
             }
         }
     }
